36da848d75db0195256aecfdfb4189448f11be6f
Клинин Станислав Андреевич <st.klinin@gmail.com>
2025-11-24 21:43:39 +0500

    feat: фикс параллельного запуска сервера и джобы,покрытие логами и настройка сборщика для докера

--- build.sbt
+++ build.sbt
@@ -1,6 +1,6 @@
 ThisBuild / version := "1"
 
-ThisBuild / scalaVersion := "3.5.2"
+ThisBuild / scalaVersion := "3.7.4"
 
 lazy val root = (project in file("."))
   .settings(
@@ -8,19 +8,25 @@ lazy val root = (project in file("."))
   )
 
 libraryDependencies ++= Seq(
-  "org.typelevel" %% "cats-effect" % "3.5.4",
-  "co.fs2" %% "fs2-core" % "3.10.2",
-  "org.http4s" %% "http4s-ember-server" % "0.23.25",
-  "org.http4s" %% "http4s-ember-client" % "0.23.25",
-  "org.http4s" %% "http4s-dsl" % "0.23.25",
+  "org.typelevel" %% "cats-effect" % "3.6.3",
+  "co.fs2" %% "fs2-core" % "3.12.2",
+  "org.http4s" %% "http4s-ember-server" % "0.23.33",
+  "org.http4s" %% "http4s-ember-client" % "0.23.33",
+  "org.http4s" %% "http4s-dsl" % "0.23.33",
   "io.prometheus" % "simpleclient" % "0.16.0",
   "io.prometheus" % "simpleclient_common" % "0.16.0",
-  "org.http4s" %% "http4s-circe" % "0.23.25",
-  "io.circe" %% "circe-core" % "0.14.7",
-  "io.circe" %% "circe-generic" % "0.14.7",
-  "io.circe" %% "circe-parser" % "0.14.7",
-  "ch.qos.logback" % "logback-classic" % "1.5.16"
+  "org.http4s" %% "http4s-circe" % "0.23.33",
+  "io.circe" %% "circe-core" % "0.14.15",
+  "io.circe" %% "circe-generic" % "0.14.15",
+  "io.circe" %% "circe-parser" % "0.14.15",
+  "ch.qos.logback" % "logback-classic" % "1.5.21",
+  "tf.tofu" %% "tofu-logging" % "0.14.0",
+  "tf.tofu" %% "tofu-logging-derivation" % "0.14.0",
+  "tf.tofu" %% "tofu-core-ce3" % "0.14.0",
 )
 
 enablePlugins(JavaAppPackaging)
 enablePlugins(DockerPlugin)
+
+dockerBaseImage := "openjdk:25-ea"
+dockerExposedPorts := Seq(8889)
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,10 @@
 import cats.effect.{IO, IOApp, Resource}
 import com.comcast.ip4s.Port
 import fs2.Stream
+import io.circe.Encoder
 import io.circe.generic.semiauto.*
-import io.circe.{Decoder, Encoder}
 import io.prometheus.client.exporter.common.TextFormat
-import io.prometheus.client.{CollectorRegistry, Counter, Gauge, Histogram}
+import io.prometheus.client.{CollectorRegistry, Counter, Gauge}
 import org.http4s.*
 import org.http4s.circe.CirceEntityCodec.*
 import org.http4s.client.Client
@@ -12,16 +12,18 @@ import org.http4s.dsl.io.*
 import org.http4s.ember.client.EmberClientBuilder
 import org.http4s.ember.server.EmberServerBuilder
 import org.http4s.headers.`Content-Type`
+import tofu.logging.Logging
+import tofu.syntax.logging.*
 
 import scala.concurrent.duration.*
 
 object Main extends IOApp.Simple {
 
-  final case class Config(
-                           oncallBaseUrl: Uri,
-                           period: FiniteDuration,
-                           proberPort: Int,
-                         )
+  private final case class Config(
+                                   oncallBaseUrl: Uri,
+                                   period: FiniteDuration,
+                                   proberPort: Int,
+                                 )
 
   final case class CreateTeamRequest(
                                       name: String,
@@ -29,15 +31,6 @@ object Main extends IOApp.Simple {
                                     )
 
   implicit val createTeamRequestEncoder: Encoder[CreateTeamRequest] = deriveEncoder
-
-  final case class Hit(value: Double)
-
-  final case class HitsResponse(hits: List[Hit])
-
-  implicit val decHit: Decoder[Hit] = Decoder.forProduct1("value")(Hit.apply)
-  implicit val dec: Decoder[HitsResponse] =
-    Decoder.forProduct1("hits")(HitsResponse.apply)
-
 
   private val registry: CollectorRegistry = new CollectorRegistry()
 
@@ -56,50 +49,95 @@ object Main extends IOApp.Simple {
       .help("HTTP request duration in seconds by endpoint")
       .register(registry)
 
-  private def metricsRoutes(reg: CollectorRegistry): HttpRoutes[IO] = HttpRoutes.of[IO] {
-    case GET -> Root / "metrics" =>
-      IO.blocking {
-        val writer = new java.io.StringWriter()
-        TextFormat.write004(writer, reg.metricFamilySamples())
-        writer.toString
-      }.flatMap { body =>
-        Ok(body).map(_.putHeaders(`Content-Type`(MediaType.text.plain)))
-      }
-  }
+  private def metricsRoutes(reg: CollectorRegistry)(using lm: Logging.Make[IO]): HttpRoutes[IO] =
+    def getMetrics = IO.blocking {
+      val writer = new java.io.StringWriter()
+      TextFormat.write004(writer, reg.metricFamilySamples())
+      writer.toString
+    }
 
-  private def probeGetTeams(client: Client[IO]): IO[Unit] = {
+    given Logging[IO] = lm.byName("MetricsLogger")
+
+    HttpRoutes.of[IO] {
+      case GET -> Root / "metrics" =>
+        for {
+          _ <- info"Got request on /metrics"
+          body <- getMetrics
+          _ <- info"Found metrics: $body"
+          response <- Ok(body).map(_.putHeaders(`Content-Type`(MediaType.text.plain)))
+          _ <- info"Sending response"
+        } yield response
+    }
+
+  private def probeGetTeams(client: Client[IO])(using lm: Logging.Make[IO]): IO[Unit] = {
+    given Logging[IO] = lm.byName("GetTeamsProber")
+
     val uri = cfg.oncallBaseUrl / "teams" / "all"
-    
+
     client.status(Request[IO](Method.GET, uri)).timed.attempt.flatMap {
       case Right((dur, status)) =>
         val seconds = dur.toNanos.toDouble / 1e9
         IO.pure(latencyGauge.set(seconds))
       case Left(_) => IO.unit
     }
+    for {
+      _ <- info"Sending GET teams request"
+      eitherGetResult <- client.status(Request[IO](Method.GET, uri)).timed.attempt
+      _ <- eitherGetResult match {
+        case Right((dur, status)) =>
+          val seconds = dur.toNanos.toDouble / 1e9
+          if (status.isSuccess) info"GET teams request completed with success in $seconds seconds" *> IO.pure(latencyGauge.set(seconds))
+          else info"GET teams request completed with failure in $seconds seconds" *> IO.unit
+        case Left(_) => info"GET teams request failed" *> IO.unit
+      }
+    } yield ()
   }
 
-  private def probeCreateTeam(client: Client[IO]): IO[Unit] = {
+  private def probeCreateTeam(client: Client[IO])(using lm: Logging.Make[IO]): IO[Unit] = {
+    given Logging[IO] = lm.byName("CreateTeamProber")
+
     val createRequest = CreateTeamRequest("MetricTestTeam")
 
     val uri = cfg.oncallBaseUrl / "teams"
 
-    client.status(Request[IO](Method.POST, uri).withEntity(createRequest)).timed.attempt.flatMap {
-      case Right((dur, status)) =>
-        val result = if (status.isSuccess) "success" else "failure"
-        IO.pure(requestCounter.labels(result).inc())
-      case Left(_) => IO.pure(requestCounter.labels("failure").inc())
-    } *> client.status(Request[IO](Method.DELETE, uri / createRequest.name)).attempt.as(())
+    for {
+      _ <- info"Sending CREATE team request"
+      eitherCreateResult <- client.status(Request[IO](Method.POST, uri).withEntity(createRequest)).attempt
+      _ <- eitherCreateResult match {
+        case Right(status) =>
+          val result = if (status.isSuccess) "success" else "failure"
+          info"CREATE team request completed with $result" *> IO.pure(requestCounter.labels(result).inc())
+        case Left(_) => info"CREATE team request failed"
+      }
+      _ <- info"Sending DELETE team request"
+      eitherDeleteResult <- client.status(Request[IO](Method.DELETE, uri / createRequest.name)).attempt
+      _ <- eitherDeleteResult match {
+        case Right(status) =>
+          val result = if (status.isSuccess) "success" else "failure"
+          info"DELETE team request completed with $result"
+        case Left(_) => info"DELETE team request failed"
+      }
+    } yield ()
   }
 
   private def probeStream(
                            client: org.http4s.client.Client[IO],
                            period: FiniteDuration
-                         ): Stream[IO, Unit] = {
-    Stream.awakeEvery[IO](period).evalMap(_ => probeGetTeams(client) *> probeCreateTeam(client))
+                         )(using lm: Logging.Make[IO]): Stream[IO, Unit] = {
+    given Logging[IO] = lm.byName("ProberJob")
+
+    Stream.awakeEvery[IO](period).evalMap(_ =>
+      for {
+        _ <- info"Probing GET teams"
+        _ <- probeGetTeams(client)
+        _ <- info"Probing CREATE team"
+        _ <- probeCreateTeam(client)
+      } yield ()
+    )
   }
 
   private val cfg: Config = Config(
-    oncallBaseUrl = Uri.unsafeFromString("http://oncall.st-ab2-klinin.ingress.sre-ab.ru"),
+    oncallBaseUrl = Uri.unsafeFromString("http://localhost:8080"),
     period = 5.seconds,
     proberPort = 8889
   )
@@ -117,16 +155,21 @@ object Main extends IOApp.Simple {
       .build
 
   def run: IO[Unit] = {
-    val app = metricsRoutes(registry).orNotFound
+    given lm: Logging.Make[IO] = Logging.Make.plain[IO]
 
-    val res =
-      for {
-        client <- clientResource
-        _ <- serverResource(app, cfg.proberPort)
-      } yield client
+    given Logging[IO] = lm.byName("ProberLogger")
 
-    res.use { client =>
-      probeStream(client, cfg.period).compile.drain
-    }
+    val metricsServer = metricsRoutes(registry).orNotFound
+
+    val app = for {
+      _ <- Stream.eval(info"Building client")
+      client <- Stream.resource(clientResource)
+      _ <- Stream.eval(info"Starting metric server")
+      server <- Stream.resource(serverResource(metricsServer, cfg.proberPort))
+      _ <- Stream.eval(info"Starting prober job")
+      _ <- probeStream(client, cfg.period)
+    } yield ()
+
+    app.compile.drain
   }
 }
